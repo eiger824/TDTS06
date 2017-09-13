7889fea39d165b14c07592547a5c04f8b86f24f5
Santi Pagola <santipagola@gmail.com>
2017-09-13 15:54:22 +0200

    Thread work in progress

--- lab2/inc/definitions.h
+++ lab2/inc/definitions.h
@@ -4,108 +4,159 @@
 #include <pthread.h>
 
 /*
-  Maximum number of bytes to write/read from the socket
-*/
+   Maximum number of bytes to write/read from the socket
+   */
 #define MAX_BUFFER_LENGTH 10000
 
 /*
-  Version of the current program
-*/
+   Version of the current program
+   */
 
 #define VERSION "0.1"
 
+#define HIGH 1
+#define LOW 0
+
 /*
-  Maximum number of simultaneous requests that our proxy can handle
-*/
+   Maximum number of simultaneous requests that our proxy can handle
+   */
 unsigned MAX_SIM_REQUESTS = 10;
 
 /*
-  Array with the created threads
-*/
+   Array with the created threads
+   */
 pthread_t *threads;
 
 /*
-  Default port number to be used
-*/
+   Default port number to be used
+   */
 int portno = 3422;
 
 /*
-  Function: get_size(thread_t p*)
-  In-param: Pointer to array of threads
-  Returns: length of the array
+   Thread data structure
+thread_id: a numerical ID assigned to the working thread
+priority: priority of that thread (to be determined)	
+*/
+
+struct thread_data
+{
+	int thread_id;
+	short priority;
+};
+
+/*
+Function: get_size(thread_t p*)
+In-param: Pointer to array of threads
+Returns: length of the array
 */
 static unsigned get_size(pthread_t *p)
 {
-   unsigned c = 0;
-   if (!p) return c;
-   while (*p)
-   {
-      ++c;
-      ++p;
-   }
-   return 0;
+	unsigned c = 0;
+	if (!p) return c;
+	while (*p)
+	{
+		++c;
+		++p;
+	}
+	return 0;
 }
 
 int parse_hostname(char* hostname, const char* buffer)
 {
-   char *substring = "Host:";
-   char *match, *colon, *cr;
-   unsigned i;
+	char *substring = "Host:";
+	char *match, *colon, *cr;
+	unsigned i;
 
-   match = strstr(buffer, substring);
-   if (match != NULL)
-   {
-      colon = strchr(match, ':');
-      if (!colon)
-      {
-         //No match, return
-         return -1;
-      }
-      cr = strchr(colon, '\r');
-      if (!cr)
-      {
-         //No match, return
-         return -1;
-      }
-   }
-   else
-   {
-      return -1;
-   }
-   //Copy the data to the hostname ptr
-   for (i=colon-match+2; i<cr-match; ++i)
-   {
-      hostname[i-(colon-match+2)] = match[i];
-   }
-   //Zero-terminate
-   hostname[cr-colon-1] = 0;
-//Last extra-check
-   if (hostname)
-   {
-      return 0;
-   }
-   else
-   {
-      return -1;
-   }
+	match = strstr(buffer, substring);
+	if (match != NULL)
+	{
+		colon = strchr(match, ':');
+		if (!colon)
+		{
+			//No match, return
+			return -1;
+		}
+		cr = strchr(colon, '\r');
+		if (!cr)
+		{
+			//No match, return
+			return -1;
+		}
+	}
+	else
+	{
+		return -1;
+	}
+	//Copy the data to the hostname ptr
+	for (i=colon-match+2; i<cr-match; ++i)
+	{
+		hostname[i-(colon-match+2)] = match[i];
+	}
+	//Zero-terminate
+	hostname[cr-colon-1] = 0;
+	//Last extra-check
+	if (hostname)
+	{
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 void hexify(char* buffer)
 {
-   unsigned i;
+	unsigned i;
 
-   for (i=0; i<strlen(buffer)-1; ++i)
-   {
-      if (buffer[i] == 0xd && buffer[i+1] == 0xa)
-      {
-         printf(" (CR + NL)\n");
-         ++i;
-      }
-      else
-      {
-         printf("%x%s", buffer[i],(i+1==strlen(buffer))?"":"-");
-      }
-   }
+	for (i=0; i<strlen(buffer)-1; ++i)
+	{
+		if (buffer[i] == 0xd && buffer[i+1] == 0xa)
+		{
+			printf(" (CR + NL)\n");
+			++i;
+		}
+		else
+		{
+			printf("%x%s", buffer[i],(i+1==strlen(buffer))?"":"-");
+		}
+	}
 }
 
+/*
+Function: print_data
+Param: buffer to print
+Returns: nothing
+*/
+
+void print_data(char* buffer, unsigned hex)
+{
+	unsigned i;
+	printf("====================Buffer====================\n");
+	if (hex)
+	{
+		hexify(buffer);
+	}
+	else
+	{
+		for (i=0; i<strlen(buffer); ++i)
+		{
+			if (32 <= buffer[i] && buffer[i] <= 127)
+			{
+				printf("%c", buffer[i]);
+			}
+			else if (buffer[i] == 9 || buffer[i] == 10 || buffer[i] == 13)
+			{
+				if (buffer[i] == 9) printf("\\t");
+				else if (buffer[i] == 10) printf("\\n\n");
+				else if (buffer[i] == 13) printf("\\r");
+			}
+			else
+			{
+				printf("%#x", buffer[i]);
+			}
+		}
+	}
+	printf("==============================================\n");
+}
 #endif /*DEFINITIONS_H_*/
--- lab2/src/proxy.c
+++ lab2/src/proxy.c
@@ -14,7 +14,16 @@
 #include "definitions.h"
 
 //Socket descriptors, made global
-int sockfd, sockfdp, newsockfd;
+int sockfd, sockfdp, newsockfd, n, c, rv, ret;
+//to use with getaddrinfo() -> host discovery
+struct addrinfo hints, *servinfo, *p;
+char buffer[MAX_BUFFER_LENGTH];
+bool hex = false;
+int signo;
+unsigned nr_active_thread = 0;
+//array of thread data structs
+struct thread_data thread_data_array[MAX_BUFFER_LENGTH];
+
 
 void help(const char* prog);
 void info();
@@ -23,237 +32,260 @@ void sig_handler(int signo);
 
 int main(int argc, char *argv[])
 {
-   int clilen, err, signo;
-   char buffer[MAX_BUFFER_LENGTH];
-   bool hex = false;
-   struct sockaddr_in serv_addr, cli_addr;
-   //to use with getaddrinfo() -> host discovery
-   struct addrinfo hints, *servinfo, *p;
-   int n,c,rv,ret;
+	int clilen, err;
+	struct sockaddr_in serv_addr, cli_addr;
 
-   //First of all, register our signal handler
-   if (signal(SIGINT, sig_handler) == SIG_ERR)
-   {
-	   perror("Error registering signal handler");
-   }
 
-   while ((c = getopt(argc,argv,"Hhm:p:v")) != -1)
-   {
-      switch(c)
-      {
-         case 'p':
-            portno = atoi(optarg);
-            break;
-         case 'm':
-            MAX_SIM_REQUESTS = atoi(optarg);
-            break;
-         case 'H':
-            hex = true;
-            break;
-         case 'h':
-            help(argv[0]);
-            exit(0);
-         case 'v':
-            info(argv[0]);
-            exit(0);
-         default:
-            fprintf(stderr, "Unknown option\n");
-            exit(1);
-      }
-   }
+	//First of all, register our signal handler
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+	{
+		perror("Error registering signal handler");
+	}
+
+	while ((c = getopt(argc,argv,"Hhm:p:v")) != -1)
+	{
+		switch(c)
+		{
+			case 'p':
+				portno = atoi(optarg);
+				break;
+			case 'm':
+				MAX_SIM_REQUESTS = atoi(optarg);
+				break;
+			case 'H':
+				hex = true;
+				break;
+			case 'h':
+				help(argv[0]);
+				exit(0);
+			case 'v':
+				info(argv[0]);
+				exit(0);
+			default:
+				fprintf(stderr, "Unknown option\n");
+				exit(1);
+		}
+	}
 
 #ifdef DEBUG_MODE
-   printf("Debug compilation flag:\t\tenabled\n");
-   printf("Selected port:\t\t\t%d\n", portno);
-   printf("Maximum simultaneous requests:\t%d\n", MAX_SIM_REQUESTS);
-   printf("Hex mode when printing data:\t%s\n", (hex)?"enabled":"disabled");
+	printf("Debug compilation flag:\t\tenabled\n");
+	printf("Selected port:\t\t\t%d\n", portno);
+	printf("Maximum simultaneous requests:\t%d\n", MAX_SIM_REQUESTS);
+	printf("Hex mode when printing data:\t%s\n", (hex)?"enabled":"disabled");
 #endif
-  
-   //Allocate memory for the threads
-   threads = malloc(MAX_SIM_REQUESTS * sizeof(pthread_t));
 
-   //Socket creation
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
-   if (sockfd < 0)
-   {
-      perror("ERROR opening socket");
-      exit(1);
-   }
+	//Allocate memory for the threads
+	threads = malloc(MAX_SIM_REQUESTS * sizeof(pthread_t));
 
-   //Initialize the data structure
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   serv_addr.sin_family = AF_INET;
-   serv_addr.sin_addr.s_addr = INADDR_ANY;
-   serv_addr.sin_port = htons(portno);
+	//Socket creation
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		perror("ERROR opening socket");
+		exit(1);
+	}
 
-   //Bind the socket with the server address
-   if (bind(sockfd, (struct sockaddr *) &serv_addr,
-            sizeof(serv_addr)) < 0)
-   {
-      perror("ERROR on binding");
-      exit(1);
-   }
+	//Initialize the data structure
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
 
-   //Put the socket on listen mode
-   if (listen(sockfd, MAX_SIM_REQUESTS) == -1)
-   {
-      perror("ERROR: could not set listen mode");
-      exit(1);
-   }
-  
-   //Init addrinfo struct
-   memset(&hints, 0, sizeof(hints));
-   hints.ai_family = AF_INET; // IPv4
-   hints.ai_socktype = SOCK_STREAM; //TCP
+	//Bind the socket with the server address
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+				sizeof(serv_addr)) < 0)
+	{
+		perror("ERROR on binding");
+		exit(1);
+	}
 
-   //Get size of the structure
-   clilen = sizeof(cli_addr);
+	//Put the socket on listen mode
+	if (listen(sockfd, MAX_SIM_REQUESTS) == -1)
+	{
+		perror("ERROR: could not set listen mode");
+		exit(1);
+	}
 
-   printf("Starting server, listening on port %d ...\n", portno);
-   while(1) {
-      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-      if (newsockfd < 0)
-      {
-         perror("ERROR on accept");
-         exit(1);
-      }
-      else
-      {
-         //Create a thread to handle the incoming connection
-         err = pthread_create(&(threads[get_size(threads)]), NULL, &handle_client, NULL);
-         if (err != 0)
-         {
-	    printf("Could not create thread :[%s]\n", strerror(err));
-         }
+	//Init addrinfo struct
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET; // IPv4
+	hints.ai_socktype = SOCK_STREAM; //TCP
+
+	//Get size of the structure
+	clilen = sizeof(cli_addr);
+
+	printf("Starting server, listening on port %d ...\n", portno);
+	while(1) {
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0)
+		{
+			perror("ERROR on accept");
+			exit(1);
+		}
+		else
+		{
+			//Create a thread to handle the incoming connection
+			thread_data_array[nr_active_thread].thread_id = nr_active_thread;
+			thread_data_array[nr_active_thread].priority = HIGH;
+			err = pthread_create(&(threads[get_size(threads)]), NULL,
+					&handle_client, &thread_data_array[nr_active_thread]);
+			if (err != 0)
+			{
+				printf("Could not create thread :[%s]\n", strerror(err));
+			}
 #ifdef DEBUG_MODE
-         else
-         {
-	    printf("Thread created successfully. Starting request handling\n");
-         }
+			else
+			{
+				printf("Thread created successfully. Starting request handling\n");
+			}
 #endif
-      }
-      bzero(buffer,MAX_BUFFER_LENGTH);
-      n = read(newsockfd,buffer,MAX_BUFFER_LENGTH-1);
-      if (n < 0) fprintf(stderr, "ERROR reading from socket\n");
+			//add up counter
+			++nr_active_thread;
+		}
+	}
 
-#ifdef DEBUG_MODE
-      printf("Received data: [");
-      if (hex)
-         hexify(buffer);
-      else
-         printf("%s", buffer);
-      printf("]\n");
-#endif
-   
-      char *hostname = malloc(200);
-      if ((ret = parse_hostname(hostname, buffer)) != -1)
-      {
-#ifdef DEBUG_MODE
-         printf("The hostname extracted is [%s]\n", hostname);
-#endif
-      }
-      else
-      {
-         fprintf(stderr, "Error parsing hostname\n");
-      }
-    
-      //Get address info of the parsed hostname
-      if ((rv = getaddrinfo(hostname, "http", &hints, &servinfo)) != 0)
-      {
-         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-         exit(1);
-      }
-  
-
-      // loop through all the results and connect to the first we can
-      for(p = servinfo; p != NULL; p = p->ai_next) {
-         if ((sockfdp = socket(p->ai_family, p->ai_socktype,
-                               p->ai_protocol)) == -1) {
-            perror("socket");
-            continue;
-         }
-
-         if (connect(sockfdp, p->ai_addr, p->ai_addrlen) == -1) {
-            perror("connect");
-            close(sockfdp);
-            continue;
-         }
-
-         break; // if we get here, we must have connected successfully
-      }
-
-      if (p == NULL) {
-         // looped off the end of the list with no connection
-         perror("failed to connect");
-         exit(2);
-      }
-
-
-      /*
-        Action!
-      */
-
-      if ((n = write(sockfdp, buffer, strlen(buffer))) < 0 )
-      {
-         perror("Write");
-      }
-#ifdef DEBUG_MODE
-      else
-      {
-         printf("(%d bytes written)\n", n);
-      }
-#endif
-
-      /*
-        Read response from server after cleaning sending buffer
-      */
-      memset(buffer, 0, strlen(buffer));
-      if ((n = read(sockfdp, buffer, MAX_BUFFER_LENGTH)) < 0)
-      {
-         perror("Error reading from socket");
-      }
-#ifdef DEBUG_MODE
-      else
-      {
-         printf("Received %d bytes from server", n);
-      }
-#endif
-   }
-  
-   freeaddrinfo(servinfo); // all done with this structure
-   return 0; 
+	freeaddrinfo(servinfo); // all done with this structure
+	return 0; 
 }
 
 void help(const char* prog)
 {
-   printf("USAGE: %s [ARGS]\n", prog);
-   printf("Args:\n");
-   printf("-h\tShow this help and exit\n");
-   printf("-H\tShow the HTTP data in hex format\n");
-   printf("-m max  Specify the maximum number of simultaneous connections the proxy\n");
-   printf("        can have (defaults to 10)\n");
-   printf("-p port Specify the listening port (defaults to %d)\n", portno);
-   printf("-v\tShow version & authors and exit\n");
+	printf("USAGE: %s [ARGS]\n", prog);
+	printf("Args:\n");
+	printf("-h\tShow this help and exit\n");
+	printf("-H\tShow the HTTP data in hex format\n");
+	printf("-m max  Specify the maximum number of simultaneous connections the proxy\n");
+	printf("        can have (defaults to 10)\n");
+	printf("-p port Specify the listening port (defaults to %d)\n", portno);
+	printf("-v\tShow version & authors and exit\n");
 }
 
 void info()
 {
-   printf("Proxy, version %s, by Goutam Bhat (goubh275) and Santiago Pagola (sanpa993)\n", VERSION);
+	printf("Proxy, version %s, by Goutam Bhat (goubh275) and Santiago Pagola (sanpa993)\n", VERSION);
 }
 
 void *handle_client(void *arg)
 {
-   pthread_t id = pthread_self();
+	pthread_t id = pthread_self();
+	struct thread_data *data;
+	data = (struct thread_data*) arg;
+	int idt = data->thread_id;
+	printf("Thread id: %d, priority: [%s]\n", idt,
+			(data->priority)?"HIGH":"LOW");
+
+
+	bzero(buffer,MAX_BUFFER_LENGTH);
+	n = read(newsockfd,buffer,MAX_BUFFER_LENGTH-1);
+	if (n < 0) fprintf(stderr, "ERROR reading from socket\n");
+
 #ifdef DEBUG_MODE
-   printf("Thread id: %lu\n", id);
+		printf("(->)Client sends data:\n");
+		print_data(buffer, hex);
 #endif
+
+		char *hostname = malloc(200);
+		if ((ret = parse_hostname(hostname, buffer)) != -1)
+		{
+#ifdef DEBUG_MODE
+			printf("The hostname extracted is [%s]\n", hostname);
+#endif
+		}
+		else
+		{
+			fprintf(stderr, "Error parsing hostname\n");
+		}
+
+		//Get address info of the parsed hostname
+		if ((rv = getaddrinfo(hostname, "http", &hints, &servinfo)) != 0)
+		{
+			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+			exit(1);
+		}
+
+
+		// loop through all the results and connect to the first we can
+		for(p = servinfo; p != NULL; p = p->ai_next) {
+			if ((sockfdp = socket(p->ai_family, p->ai_socktype,
+							p->ai_protocol)) == -1) {
+				perror("socket");
+				exit(1);
+			}
+
+			if (connect(sockfdp, p->ai_addr, p->ai_addrlen) == -1) {
+				perror("connect");
+				close(sockfdp);
+				exit(1);
+			}
+
+			break; // if we get here, we must have connected successfully
+		}
+
+		if (p == NULL) {
+			// looped off the end of the list with no connection
+			perror("failed to connect");
+			exit(2);
+		}
+
+
+		/*
+		   Write the request to the server
+		   */
+		if ((n = write(sockfdp, buffer, strlen(buffer))) < 0 )
+		{
+			perror("Write");
+		}
+#ifdef DEBUG_MODE
+		else
+		{
+			printf("(->)Proxy forwards data (%d bytes)\n", n);
+	}
+#endif
+
+	/*
+	   Read response from server after cleaning sending buffer
+	   */
+	memset(buffer, 0, strlen(buffer));
+#ifdef DEBUG_MODE
+	printf("Now waiting for server to respond...\n");
+#endif
+	if ((n = read(sockfdp, buffer, MAX_BUFFER_LENGTH)) < 0)
+	{
+		perror("Error reading from socket");
+	}
+#ifdef DEBUG_MODE
+	else
+	{
+		printf("(<-)Received %d bytes from server:\n", n);
+		print_data(buffer, hex);
+		printf("(<-)Proxy forwards data back to client (expected %d bytes)\n", strlen(buffer));
+		//Send it back to the client
+		if ((n = write(newsockfd, buffer, strlen(buffer))) < 0)
+		{
+			perror("Write");
+		}
+#ifdef DEBUG_MODE
+		else
+		{
+			printf("Success! Wrote %d bytes back to the client\n", n);
+			close(newsockfd);
+		}
+#endif
+
+	}
+#endif
+
+	//Exit thread
+	pthread_exit(NULL);
+
+
 }
 
 void sig_handler(int signo)
 {
 	if (signo == SIGINT)
 	{
-		printf("\rReceived SIGINT, Exiting...\n");
+		printf("\rReceived SIGINT, Exiting program ...\n");
 		if (sockfd >= 0) close(sockfd);
 		if (newsockfd >= 0) close(newsockfd);
 		if (sockfdp >= 0) close(sockfdp);
